c9485510c14a31ab3132fee52526d8f23dbcc175
marc-armillas <marcarmillas@gmail.com>
2021-12-04 11:18:28 +0100

    npm dependencies fixed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
+//variables
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require('./keys').mongoURI;
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+
+
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+//routes
+app.use('/cities', require('./routes/cities'))
+
+//mongoose connect with db
+const mongoose = require("mongoose");
+
+mongoose.connect(db)
+    .then(() => console.log('Connection to Mongo DB established'))
+    .catch(err => console.log(err));
