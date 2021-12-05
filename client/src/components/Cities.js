80fc12715fd03f9a8e6002e7994ff73d5a9cdce3
marc-armillas <marcarmillas@gmail.com>
2021-12-05 18:40:10 +0100

    add db results render

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 const Cities = () =>{
     const url = 'http://localhost:5000/';
-    const [data, getData] = useState('');
+    const [data, setData] = useState('');
 
     useEffect(() => {
         getAllData()
@@ -14,13 +14,22 @@ const Cities = () =>{
         .then((response) => {
             const allCities = response.data;
             //add data to state
-            getData(allCities)
+            setData(allCities)
     })
     .catch(error => console.error(`Error: ${error}`));
     }
     console.log(data)
     return(
-        <h1>hjolalal</h1>
+        Object.keys(data).map((x) => {
+            return(
+                <ul key={data[x]._id}>
+                    <li>{data[x].name}</li>
+                    <li>{data[x].country}</li>
+                    <li>{data[x]._id}</li>
+                    <li>{data[x].image}</li>
+                </ul>
+        )
+        })
     )
 };
 
