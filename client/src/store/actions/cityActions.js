e188fda2990c17d8150f6149b6885d8ac01dc4ce
marc-armillas <marcarmillas@gmail.com>
2021-12-14 12:40:23 +0100

    first redux commit up to task 5 working perfectly

---
+++ client/src/store/actions/cityActions.js
@@ -0,0 +1,22 @@
+// import axios from 'axios'
+// import { useState } from 'react';
+
+
+
+// const GetAllData = () => {
+
+//     const url = 'http://localhost:5000/';
+//     const [data, setData] = useState('');
+// return(
+//     axios.get(`${url}cities/all`)
+//     .then((response) => {
+//         const allCities = response.data;
+//         //add data to state
+//         setData(allCities)
+//     })
+//     .catch(error => console.error(`Error: ${error}`))
+//     );
+// };
+
+
+// export default GetAllData();
